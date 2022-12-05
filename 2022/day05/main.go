6878968961c75552f457c4064801482ab4ebe56d
Fatme <hfatme@gmail.com>
2022-12-05 18:51:07 +0200

    Improve day05 solution

--- 2022/day05/main.go
+++ 2022/day05/main.go
@@ -9,62 +9,71 @@ import (
 	"github.com/emirpasic/gods/lists/arraylist"
 )
 
-const ROWS = 10 // 4
-const COLS = 9  // 3
-
+const COLS = 9
 const INPUT_FILE = "input.txt"
 
-var data [COLS]*arraylist.List
+func parseInput(lines []string) [COLS]*arraylist.List {
+	var data [COLS]*arraylist.List
+
+	for _, line := range lines {
+		if !isInstructionLine(line) && line != "" && !strings.Contains(line, " 1   2") {
+			col := 0
+
+			for j := 0; j < len(line); j += 4 {
+				item := string(line[j+1])
+				// Ignore empty cells
+				if item == " " {
+					col++
+					continue
+				}
+
+				if data[col] == nil {
+					data[col] = arraylist.New()
+				}
+
+				data[col].Insert(0, item)
+				col++
+			}
+		}
+	}
+
+	return data
+}
+
+func isInstructionLine(line string) bool {
+	return strings.Contains(line, "move")
+}
+
+func parseInstructionLine(line string) (int, int, int) {
+	parts := strings.Split(line, " ")
+
+	movesCount, _ := strconv.Atoi(parts[1])
+	from, _ := strconv.Atoi(parts[3])
+	to, _ := strconv.Atoi(parts[5])
+
+	return movesCount, from, to
+}
 
 func main() {
-	instructionsLine := 0
-
 	input, _ := os.ReadFile(INPUT_FILE)
 
 	lines := strings.Split(string(input), "\n")
 
-	for index, line := range lines {
-		if index == ROWS-2 {
-			instructionsLine = index + 1
-			break
-		}
+	data := parseInput(lines)
 
-		col := 0
+	for _, line := range lines {
+		if isInstructionLine(line) {
+			movesCount, from, to := parseInstructionLine(line)
 
-		for j := 0; j < len(line); j += 4 {
-			item := string(line[j+1])
-			// Ignore empty cells
-			if item == " " {
-				col++
-				continue
+			fromList := data[from-1]
+			toList := data[to-1]
+
+			for i := movesCount - 1; i >= 0; i-- {
+				fromIndex := fromList.Size() - 1
+				item, _ := fromList.Get(fromIndex)
+				fromList.Remove(fromIndex)
+				toList.Add(item)
 			}
-
-			if data[col] == nil {
-				data[col] = arraylist.New()
-			}
-
-			data[col].Insert(0, item)
-			col++
-		}
-	}
-
-	for index := instructionsLine + 1; index < len(lines); index++ {
-		line := lines[index]
-
-		parts := strings.Split(line, " ")
-
-		countToMove, _ := strconv.Atoi(parts[1])
-		from, _ := strconv.Atoi(parts[3])
-		to, _ := strconv.Atoi(parts[5])
-
-		fromList := data[from-1]
-		toList := data[to-1]
-
-		for i := countToMove - 1; i >= 0; i-- {
-			fromIndex := fromList.Size() - 1
-			item, _ := fromList.Get(fromIndex)
-			fromList.Remove(fromIndex)
-			toList.Add(item)
 		}
 	}
 
