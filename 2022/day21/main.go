46fa3007dda95e5c700b61cf2b2b04bf61d08527
Fatme <hfatme@gmail.com>
2022-12-21 22:21:02 +0200

    Solve day21 part2

--- 2022/day21/main.go
+++ 2022/day21/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -14,26 +15,6 @@ type Monkey struct {
 	operator  string
 	monkey1   string
 	monkey2   string
-}
-
-func (monkey *Monkey) setValues() {
-	operator := ""
-
-	if strings.Contains(monkey.operation, "+") {
-		operator = "+"
-	} else if strings.Contains(monkey.operation, "-") {
-		operator = "-"
-	} else if strings.Contains(monkey.operation, "*") {
-		operator = "*"
-	} else if strings.Contains(monkey.operation, "/") {
-		operator = "/"
-	}
-
-	parts := strings.Split(monkey.operation, fmt.Sprintf(" %s ", operator))
-
-	monkey.monkey1 = parts[0]
-	monkey.monkey2 = parts[1]
-	monkey.operator = operator
 }
 
 func (monkey *Monkey) process() {
@@ -61,9 +42,28 @@ func (monkey *Monkey) process() {
 	}
 }
 
+func (monkey *Monkey) getValue() float64 {
+	if isNumber(monkey.operation) {
+		return float64(monkey.value)
+	}
+
+	if monkey.operator == "+" {
+		return monkeysMap[monkey.monkey1].getValue() + monkeysMap[monkey.monkey2].getValue()
+	} else if monkey.operator == "-" {
+		return monkeysMap[monkey.monkey1].getValue() - monkeysMap[monkey.monkey2].getValue()
+	} else if monkey.operator == "*" {
+		return monkeysMap[monkey.monkey1].getValue() * monkeysMap[monkey.monkey2].getValue()
+	} else if monkey.operator == "/" {
+		return monkeysMap[monkey.monkey1].getValue() / monkeysMap[monkey.monkey2].getValue()
+	}
+
+	panic("unexpected")
+}
+
 var monkeysMap = map[string]*Monkey{}
 
 const rootMonkey = "root"
+const myMonkey = "humn"
 
 func main() {
 	input, _ := os.ReadFile("input.txt")
@@ -78,14 +78,19 @@ func main() {
 			monkey.operation = strconv.Itoa(num)
 			monkey.value = num
 		} else {
+			data := strings.Split(parts[1], " ")
+
+			monkey.monkey1 = data[0]
+			monkey.monkey2 = data[2]
+			monkey.operator = data[1]
 			monkey.operation = parts[1]
-			monkey.setValues()
 		}
 
 		monkeysMap[monkeyName] = monkey
 	}
 
-	solvePart1()
+	// solvePart1()
+	solvePart2()
 }
 
 func solvePart1() {
@@ -96,6 +101,34 @@ func solvePart1() {
 	}
 
 	fmt.Println(monkeysMap[rootMonkey].value)
+}
+
+func solvePart2() {
+	var min = 0
+	var max = math.MaxInt
+
+	monkey := monkeysMap[rootMonkey]
+
+	for min < max {
+		mid := (min + max) / 2
+
+		monkeysMap[myMonkey].value = mid
+		monkeysMap[myMonkey].operation = strconv.Itoa(mid)
+
+		value1 := monkeysMap[monkey.monkey1].getValue()
+		value2 := monkeysMap[monkey.monkey2].getValue()
+
+		if value1 == value2 {
+			fmt.Println(mid)
+			return
+		}
+
+		if value1 > value2 {
+			min = mid + 1
+		} else {
+			max = mid
+		}
+	}
 }
 
 func isNumber(str string) bool {
